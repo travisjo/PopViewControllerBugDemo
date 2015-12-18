e468231c17e93df158d781c75cede3e5ebc0cdde
travis.jo <tjohnson@summitesp.com>
2015-12-18 13:32:01 -0600

    Initial Commit

---
+++ PopViewControllerBugDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopViewControllerBugDemo
+//
+//  Created by Travis Johnson on 12/18/15.
+//  Copyright © 2015 Travis Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopViewControllerBugDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopViewControllerBugDemo
+//
+//  Created by Travis Johnson on 12/18/15.
+//  Copyright © 2015 Travis Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
